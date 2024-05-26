96eeae26523d8de5a8feada86d6a941ee74b9b6f
Nitro <fridaynitro@gmail.com>
2024-05-26 12:18:54 +0000

    Initial commit with App.js

---
+++ NitroApp/App.js
@@ -0,0 +1,27 @@
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View } from 'react-native';
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+    <Text style={styles.text}>
+      My name is <Text style={styles.boldText}>Kofi Asamoah</Text>
+    </Text>
+  </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#ADD8E6',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  text: {
+    fontSize: 24, 
+  },
+  boldText: {
+    fontWeight: 'bold', 
+  },
+});
